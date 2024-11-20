a8d1889372ba66770386a707df3d38ad03dbc468
Douglas Domingos <doug.domingossilva@gmail.com>
2024-11-20 16:46:57 -0300

    build: adiciona Action para deploy no GitHub

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://guardians-dsc.github.io',
+  base: 'iniciativa-devops',
   integrations: [tailwind()]
 });
